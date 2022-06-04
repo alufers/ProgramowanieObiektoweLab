d395fee1f24cb2269ba3e2902c31ff8e94379cd2
alufers <alufers@wp.pl>
2022-06-04 12:39:27 +0200

    add lab7

---
+++ lab7/lab7/lab7/Model.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+public class BloggingContext : DbContext
+{
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Post> Posts { get; set; }
+
+    public string ConnectionString { get; }
+
+    public BloggingContext(string connectionString)
+    {
+        this.ConnectionString = connectionString;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlServer(this.ConnectionString);
+    }
+}
+
+public class TasksContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+
+    public DbSet<Role> Roles { get; set; }
+
+    public DbSet<Task> Tasks { get; set; }
+
+    public string ConnectionString { get; }
+
+    public TasksContext(string connectionString)
+    {
+        this.ConnectionString = connectionString;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlServer(this.ConnectionString);
+    }
+}
+
+
+public class Blog
+{
+    public long Id { get; set; }
+    public string Url { get; set; }
+
+    public List<Post> Posts { get; } = new();
+}
+
+public class Post
+{
+    public long Id { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+
+    public long BlogId { get; set; }
+    public Blog Blog { get; set; }
+}
+
+public class User
+{
+    public long Id { get; set; }
+
+    public string Username { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+}
+
+public class Role
+{
+     public long Id { get; set; }
+
+    public string Name { get; set; }
+
+    public long UserId { get; set; }
+
+    public User User { get; set; }
+
+}
+
+public class Task
+{
+    public long Id { get; set; }
+
+    public string Title { get; set; }
+
+    public string Description { get; set; }
+
+    public int Priority { get; set; }
+
+    public long UserId { get; set; }
+
+    public User User { get; set; }
+}
---
+++ lab7/lab7/lab7/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        // Hint: change `DESKTOP-123ABC\SQLEXPRESS` to your server name
+        //       alternatively use `localhost` or `localhost\SQLEXPRESS`
+
+        string connectionString = @"Data Source=localhost;Initial Catalog=blogdb;Integrated Security=True";
+
+        using (BloggingContext db = new BloggingContext(connectionString))
+        {
+            Console.WriteLine($"Database ConnectionString: {db.ConnectionString}.");
+
+            // Create
+            Console.WriteLine("Inserting a new blog");
+
+            db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+            db.SaveChanges();
+
+            // Read
+            Console.WriteLine("Querying for a blog");
+
+            Blog blog = db.Blogs
+                .OrderBy(b => b.Id)
+                .First();
+
+            // Update
+            Console.WriteLine("Updating the blog and adding a post");
+
+            blog.Url = "https://devblogs.microsoft.com/dotnet";
+            blog.Posts.Add(new Post { Title = "Hello World", Content = "I wrote an app using EF Core!" });
+            db.SaveChanges();
+
+            // Delete
+            Console.WriteLine("Delete the blog");
+
+            db.Remove(blog);
+            db.SaveChanges();
+        }
+
+        using (TasksContext db = new TasksContext(connectionString))
+        {
+            Random rand = new Random();
+            int rando = rand.Next() % 1000;
+            string[] surnames = { "Kowalski", "Nowak", "Kowal", "Janowski", "Jackowski", "Testowy"};
+            string[] firstNames = { "Jan", "Michał", "Krzysztof", "Filip", "Michalina", "Kamil", "Anna", "Stefan", "Stanisław", "Mateusz", "Łukasz" };
+            User user = new User { Username = $"user-{rando}", FirstName = firstNames[rand.Next(firstNames.Length)], LastName = surnames[rand.Next(surnames.Length)] };
+            
+            Console.WriteLine($"Creating user '{user.Username}' ({user.FirstName} {user.LastName})");
+
+            db.Add(user);
+            db.SaveChanges();
+
+            int rolesCount = rand.Next(4) + 1;
+
+            Console.WriteLine($"Adding {rolesCount} roles to the user");
+
+            for(int i = 0; i < rolesCount; i++)
+            {
+                string[] roleNames = { "Editor", "Viewer", "Cleaner", "Administrator", "Owner", "Maintainer", "Developer", "Guest", "Auditor", "Employee", "Subcontractor" };
+                Role role = new Role { Name = roleNames[rand.Next(roleNames.Length)], User = user, UserId = user.Id };
+                db.Add(role);
+                db.SaveChanges();
+            }
+
+            var users = (from c in db.Users select c).ToList();
+            Console.WriteLine("Id".PadRight(8) + "Name".PadRight(15) + "First name".PadRight(15) + "Last name".PadRight(15) + "Roles".PadRight(25));
+            foreach(var u in users)
+            {
+                var roles = (from c in db.Roles where c.UserId == u.Id select c).ToList();
+                string rolesStr = "";
+                foreach(var r  in roles)
+                {
+                    rolesStr += r.Name + ", ";
+                }
+                Console.WriteLine(u.Id.ToString().PadRight(8) + u.Username.PadRight(15) + u.FirstName.PadRight(15) + u.LastName.PadRight(15) + rolesStr.PadRight(25));
+            }
+
+        }
+    }
+}
